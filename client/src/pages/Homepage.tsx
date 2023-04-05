3baa838b8d62e7fc26d5784c4f654d570509db31
fostersoasare <fostersoasare@gmail.com>
2023-04-05 20:22:35 +0000

    templates responsiveness

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -292,13 +292,13 @@ const Homepage = () => {
 					<img src="https://lh3.googleusercontent.com/hWl6SS7ryc7NY1-OEnUKcLSosWSGWXesIVH-TmBD4AOKVfQL5MzEQuXcy8jFI5NIs2dTftP_J9BpbEJkGiP44CN2YkaRTcZUPVV6M40QLJxPj4WpJI0=w1024" alt="" />
 				</aside>
 			</section>
-			<section id="templates" className="py-24 border-border border-b-[1px]">
+			<section id="templates" className="py-24 border-border border-b-[1px] px-6">
 				<div className="w-full h-full max-w-screen-lg mx-auto flex justify-center items-center flex-col ">
-					<h1 className="text-4xl mb-4">Get a head start with templates</h1>
-					<p className="w-[82%] text-desc text-center text-lg">
+					<h1 className="text-xl sm:text-4xl mb-4">Get a head start with templates</h1>
+					<p className="w-[82%] text-desc text-center text-md sm:text-lg">
 						Choose from a variety of dashboards, project trackers, and other professionally-designed templates to kick things off quickly.
 					</p>
-					<div className="w-full h-auto flex justify-center gap-6 items-center mb-12 mt-8">
+					<div className="w-full h-auto grid grid-cols-2 md:grid-cols-4 justify-center gap-6 items-center mb-12 mt-8">
 						{templates.map((template: templateTypes, index: number) => (
 							<div key={index} className="">
 								<p className="text-center mb-2 font-medium">{template.name}</p>
