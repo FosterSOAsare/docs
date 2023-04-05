d9dda03bc921f232fcbf9c35f5f9eb13891dbdca
fostersoasare <fostersoasare@gmail.com>
2023-04-05 20:14:57 +0000

    Footer section responsiveness
    Plans responsiveness in progress

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -126,7 +126,7 @@ const Homepage = () => {
 				</aside>
 			</section>
 
-			<section className="bg-background2 w-full h-auto advantages px-12 py-20 ">
+			<section className="bg-background2 w-full h-auto advantages px-6 sm:px-12 py-20 ">
 				<div className="max-w-screen-lg mx-auto w-full flex-col sm:flex-row h-full flex justify-between items-center gap-12">
 					{advantages.map((advantage: advantagesType, index: number) => (
 						<HomepageAdvantage key={index} {...advantage} />
@@ -192,57 +192,78 @@ const Homepage = () => {
 				</aside>
 			</section>
 
-			<section className="my-24 " id="plans">
+			<section className="my-24 md:px-10 px-4" id="plans">
 				<div className="w-full mx-auto h-full max-w-screen-lg flex items-center justify-center flex-col">
-					<h2 className="text-4xl mb-4">Find the plan that’s right for you</h2>
-					<h3 className="text-xl font-medium mb-4">Google Docs is a part of Google Workspace</h3>
+					<h2 className="text-2xl w-[80%] text-center md:text-4xl mb-4">Find the plan that’s right for you</h2>
+					<h3 className="text-md w-[80%] text-center md:text-xl font-medium mb-4">Google Docs is a part of Google Workspace</h3>
 					<p className="font-medium text-md">Every plan includes</p>
-					<article className="h-[70px]  my-4 flex justify-center gap-6">
-						{addedSoftwares.map((software: AddOnsTypes, index: number) => (
-							<div key={index}>
-								<img src={software.image} alt="" className="block mx-auto mb-2" />
-								<p className="text-[12px] text-center">{software.name}</p>
-							</div>
-						))}
+					<article className="h-auto sm:h-[70px]  my-4 flex flex-col sm:flex-row justify-center gap-6">
+						<div className="flex justify-center md:gap-6 gap-4">
+							{addedSoftwares.map((software: AddOnsTypes, index: number) => {
+								if (index <= 6) {
+									return (
+										<div key={index}>
+											<img src={software.image} alt="" className="block mx-auto mb-2" />
+											<p className="text-[12px] text-center hidden sm:block">{software.name}</p>
+										</div>
+									);
+								}
+							})}
+						</div>
+						<div className="flex justify-center gap-4 md:gap-6">
+							{addedSoftwares.map((software: AddOnsTypes, index: number) => {
+								if (index > 6) {
+									return (
+										<div key={index}>
+											<img src={software.image} alt="" className="block mx-auto mb-2" />
+											<p className="text-[12px] text-center hidden sm:block">{software.name}</p>
+										</div>
+									);
+								}
+							})}
+						</div>
 					</article>
 					<article className="w-full">
 						<section className="w-full h-[270px] flex justify-between ">
-							<div className="w-[40%] border-border border-r-[1px] h-full"></div>
-							<div className="w-[30%] border-border border-[1px] h-full border-l-0 p-6 flex items-center flex-col">
-								<h3 className="text-xl mb-24"> For Personal (Free)</h3>
+							<div className="w-[50%] sm:w-[40%] border-border border-r-[1px] h-full"></div>
+							<div className="w-[25%] sm:w-[30%] border-border border-[1px] h-full border-l-0 p-6 flex items-center flex-col relative bg-red-300">
+								<h3 className="text-xl -rotate-90 md:w-full w-auto bg-green-400 bottom-8  md:bottom-0 md:rotate-0 sm:bottom-26 sm:bottom-12 absolute md:relative">
+									{" "}
+									For Personal <span className="hidden md:block"> (Free)</span>{" "}
+								</h3>
 
-								<a className="text-secondary flex justify-center items-center px-6 font-medium h-12 rounded-[3px] hover:cursor-pointer border-border hover:border-secondary border-[1px]">
+								<a className="hidden text-secondary md:flex justify-center items-center px-6 font-medium h-12 rounded-[3px] hover:cursor-pointer border-border hover:border-secondary border-[1px]">
 									Go to Docs
 								</a>
 							</div>
-							<div className="w-[30%] border-border border-[1px] h-full border-l-0 p-6 flex items-center flex-col">
-								<h3 className="text-xl mb-2">Business Standard</h3>
-								<h1 className="text-4xl text-secondary">$12 USD</h1>
-								<p className="text-center text-sm text-desc">per user / month, 1 year commitment</p>
-								<a className="text-white flex justify-center items-center px-6 font-medium h-12 rounded-[3px] bg-secondary mt-6 mb-4 hover:cursor-pointer">Get Started</a>
+							<div className="w-[25%] sm:w-[30%] border-border border-[1px] h-full border-l-0 p-6 flex items-center flex-col relative">
+								<h3 className="text-xl -rotate-90 md:w-full sm:w-auto  md:bottom-0 md:rotate-0 bottom-8 bg-green-400 sm:bottom-20 absolute md:relative">Business Standard</h3>
+								<h1 className="hidden text-4xl text-secondary">$12 USD</h1>
+								<p className="hidden md:block text-center text-sm text-desc">per user / month, 1 year commitment</p>
+								<a className="hidden text-white md:flex justify-center items-center px-6 font-medium h-12 rounded-[3px] bg-secondary mt-6 mb-4 hover:cursor-pointer">Get Started</a>
 								<a
 									href="https://workspace.google.com/intl/en/pricing.html?utm_source=docsforwork&utm_medium=et&utm_content=seemoreplans&utm_campaign=pricing"
 									target="_blank"
 									rel="noreffer"
-									className="text-secondary font-medium">
+									className="hidden text-secondary font-medium">
 									See more plans
 								</a>
 							</div>
 						</section>
 						{planFeatures.map((planFeature: planFeaturesType, index: number) => {
 							return (
-								<article key={index} className="flex justify-between items-center h-16 border-border border-b-[1px]">
-									<div className="w-[40%] h-full border-border border-r-[1px] flex justify-start px-2 items-center">{planFeature.name}</div>
-									<div className="w-[30%] h-full border-border border-r-[1px] flex justify-center items-center">
+								<article key={index} className="flex justify-between items-center h-28  sm:h-16 border-border border-b-[1px]">
+									<div className="w-[50%] sm:w-[40%] h-full border-border border-r-[1px] flex justify-start px-2 items-center">{planFeature.name}</div>
+									<div className="w-[25%] sm:w-[30%] h-full border-border border-r-[1px] flex justify-center items-center">
 										{typeof planFeature.free === "string" ? (
-											<p className="w-[90%] text-center font-medium">{planFeature.free}</p>
+											<p className="w-[90%] text-xs sm:text-sm text-center font-medium">{planFeature.free}</p>
 										) : (
 											<planFeature.free size={24} color={planFeature?.free?.name === "HiOutlineCheck" ? "#185ABC" : "#000000"} />
 										)}
 									</div>
-									<div className="w-[30%] h-full border-border border-r-[1px] flex justify-center items-center">
+									<div className="w-[25%] sm:w-[30%] h-full border-border border-r-[1px] flex justify-center items-center">
 										{typeof planFeature.paid === "string" ? (
-											<p className="w-[90%] text-center font-medium">{planFeature.paid}</p>
+											<p className="w-[90%] text-xs sm:text-sm text-center font-medium">{planFeature.paid}</p>
 										) : (
 											<planFeature.paid color={planFeature?.paid?.name === "HiOutlineCheck" ? "#185ABC" : "#000000"} size={24} />
 										)}
@@ -321,16 +342,16 @@ const Homepage = () => {
 				</aside>
 			</section>
 
-			<footer className="bg-background2 w-full h-auto py-12">
+			<footer className="bg-background2 w-full h-auto py-12 px-6">
 				<div className="w-full h-full max-w-screen-lg  mx-auto">
-					<article className="flex justify-start items-center gap-6 pb-4 border-b-[1px] border-border">
+					<article className="flex justify-start items-start sm:items-center gap-3 sm:gap-6 pb-4 border-b-[1px] border-border flex-col sm:flex-row">
 						<p className="font-medium">
 							Follow our{" "}
 							<a href="" className="text-secondary">
 								Blog
 							</a>
 						</p>
-						<nav className="flex justify-start gap-2">
+						<nav className="flex justify-start gap-2 ">
 							<a
 								className="w-12 h-12 rounded-full hover:bg-border flex items-center justify-center"
 								href="https://www.youtube.com/channel/UCBmwzQnSoj9b6HzNmFrg_yw"
@@ -346,13 +367,13 @@ const Homepage = () => {
 							</a>
 						</nav>
 					</article>
-					<article className="flex justify-between items-center pt-4">
-						<div className="flex justify-start items-center gap-12 py-4">
+					<article className="flex justify-between  md:items-center pt-4 flex-col md:flex-row ">
+						<div className="flex justify-start flex-col sm:flex-row  sm:items-center gap-6 sm:gap-12 py-4">
 							<a href="" className="text-xl">
 								Google
 							</a>
 
-							<nav className="flex justify-start  w-auto  gap-6">
+							<nav className="flex justify-start flex-col sm:flex-row  w-auto  gap-6">
 								<a className="font-medium" href="https://www.youtube.com/channel/UCBmwzQnSoj9b6HzNmFrg_yw" target="_blank" rel="noreferrer">
 									About Google
 								</a>
@@ -367,7 +388,7 @@ const Homepage = () => {
 								</a>
 							</nav>
 						</div>
-						<div className="flex justify-start items-center gap-12 py-4  w-[30%]">
+						<div className="flex justify-start items-center gap-12 py-4 w-full sm:w-[70%] md:w-[30%]">
 							<nav className="flex justify-start  w-full gap-6 ">
 								<a className="font-medium" href="https://www.youtube.com/channel/UCBmwzQnSoj9b6HzNmFrg_yw" target="_blank" rel="noreferrer">
 									Help
