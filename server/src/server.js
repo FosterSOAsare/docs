0de7cb94205831329293c0e94a0fe65f3dcc415c
fostersoasare <fostersoasare@gmail.com>
2023-04-27 11:18:04 +0000

    Project refactoring

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,14 @@
-import http from 'http'
-import app from './app'
-import dotenv from 'dotenv'
+const http = require("http");
+const app = require("./app");
+const dotenv = require("dotenv");
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
-
 (async function () {
-  server.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}...`)
-  })
-}())
+	server.listen(PORT, () => {
+		console.log(`Listening on PORT ${PORT}...`);
+	});
+})();
+
+// console.log("dead");
